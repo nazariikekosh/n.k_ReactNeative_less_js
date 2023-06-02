414b6e90e710aa6a070a85b1c10d1b6a95be9f1b
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-02 14:00:07 +0300

    add__components-Header

--- lessons/App.js
+++ lessons/App.js
@@ -1,33 +1,14 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import Header from './src/components/uikit/header';
 
 export default class App extends Component {
+  state = {
+    title: 'STAR GATE'
+  }
   render() {
     return (
-      <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>STAR GATE</Text>
-      </View>
-    );
+      <Header title={this.state.title} />
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  viewStyle: {
-    backgroundColor: '#30d0fe',
-    height: 116,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    paddingLeft: 22,
-    paddingTop: 71,
-    shadowColor: '#000',
-    textShadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.2,
-    elevation: 2,
-    position: 'relative',
-  },
-  textStyle: {
-    color: '#fff',
-    fontSize: 28,
-    fontFamily: 'AvenirNext-DemiBold',
-  },
-});
---
+++ lessons/src/components/uikit/header.js
@@ -0,0 +1,37 @@
+import react from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = ({title}) => {
+    return (
+<View style={styles.viewStyle}>
+        <Text style={styles.textStyle}>{title}</Text>
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewStyle: {
+      backgroundColor: '#30d0fe',
+      height: 116,
+      justifyContent: 'center',
+      alignItems: 'flex-start',
+      paddingLeft: 22,
+      paddingTop: 71,
+      shadowColor: '#000',
+      textShadowOffset: {width: 0, height: 2},
+      shadowOpacity: 0.2,
+      elevation: 2,
+      position: 'relative',
+    },
+    textStyle: {
+      color: '#fff',
+      fontSize: 28,
+      fontFamily: 'AvenirNext-DemiBold',
+    },
+  });
+  
+
+
+
+
+export default Header;
